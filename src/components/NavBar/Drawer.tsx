cc9491a4d655ec7a16a2ff4fe346bcb41e36da82
Mateusz Chochol <mch.priv@gmail.com>
2020-11-17 21:50:52 +0100

    Replacing drawer list with tabs

--- src/components/NavBar/Drawer.tsx
+++ src/components/NavBar/Drawer.tsx
@@ -5,19 +5,31 @@ import {
   Theme
 } from '@material-ui/core/styles';
 import {
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
+  useSelector,
+  useDispatch
+} from 'react-redux';
+import {
+  getTab,
+  setTab,
+} from '../../redux/TabsSlice';
+import {
   SwipeableDrawer,
+  Tabs,
+  Tab,
+  Box,
+  Grid,
 } from '@material-ui/core';
 import AppBarMenuItem from '../../types/AppBarMenuItem';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    list: {
+    tabs: {
       width: 250,
+      marginTop: 15,
     },
+    tabBox: {
+      fontSize: 15
+    }
   }),
 );
 
@@ -28,6 +40,8 @@ const Drawer: React.FC<{
 }> = ({ items, open, setOpen }) => {
   const classes = useStyles();
   const iOS = Boolean(process.env.browser && /iPad|iPhone|iPod/.test(navigator.userAgent));
+  const tabIndex = useSelector(getTab)
+  const dispatch = useDispatch();
 
   return (
     <SwipeableDrawer
@@ -38,18 +52,33 @@ const Drawer: React.FC<{
       disableBackdropTransition={!iOS}
       disableDiscovery={iOS}
     >
-      <List className={classes.list}>
-        {items.map(item => {
-          return (
-            <ListItem button key={item.key}>
-              <ListItemIcon>
-                {item.icon}
-              </ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItem>
-          )
-        })}
-      </List>
+      <Tabs
+        className={classes.tabs}
+        orientation="vertical"
+        value={tabIndex}
+        indicatorColor="secondary"
+        variant="fullWidth"
+        scrollButtons="on"
+        onChange={(event: React.ChangeEvent<{}>, tabIndex: number) => dispatch(setTab(tabIndex))}
+      >
+        {items.map(item =>
+          <Tab
+            key={item.key}
+            label={
+              <Grid container justify='flex-start' alignItems="center">
+                <Grid item xs={3}>
+                  <Box display='flex'>{item.icon}</Box>
+                </Grid>
+                <Grid item xs={9}>
+                  <Box display='flex' className={classes.tabBox}>
+                    {item.text}
+                  </Box>
+                </Grid>
+              </Grid>
+            }
+          />
+        )}
+      </Tabs>
     </SwipeableDrawer>
   )
 }
--- src/components/NavBar/MenuAppBar.tsx
+++ src/components/NavBar/MenuAppBar.tsx
@@ -146,7 +146,7 @@ const MenuAppBar: React.FC<WithWidth> = ({ width }) => {
                 <Drawer items={menuItems} open={openDrawerMenu} setOpen={setOpenDrawerMenu} />
               </Grid>
             }
-            <Grid item xs={5} md={2}>
+            <Grid item xs={6} md={2}>
               <Typography align='left'>
                 <Button
                   size="large"
@@ -176,7 +176,7 @@ const MenuAppBar: React.FC<WithWidth> = ({ width }) => {
                 </Tabs>
               </Grid>
             }
-            <Grid item xs={6} sm={5} md={2} >
+            <Grid item xs={4} md={2} >
               <AccountOptions />
             </Grid>
           </Grid>
