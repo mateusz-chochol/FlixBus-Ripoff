635bc74d2ed2bca92fc27b8663453cbe1766eb75
Mateusz Chochol <mch.priv@gmail.com>
2021-04-05 01:14:59 +0200

    Adding white background

--- src/components/ResultsPage/ResultsPage.tsx
+++ src/components/ResultsPage/ResultsPage.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Paper } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 
 const ResultsPage: React.FC = () => {
   const history = useHistory();
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      width="100vw"
-    >
-      <Typography variant='h1'>
-        <Box alignSelf='center' justifySelf='center'>Results page</Box>
-      </Typography>
-    </Box>
+    <Paper square>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="92vh"
+        width="100vw"
+      >
+        <Typography variant='h1'>
+          <Box alignSelf='center' justifySelf='center'>Results page</Box>
+        </Typography>
+      </Box>
+    </Paper>
   )
 }
 
