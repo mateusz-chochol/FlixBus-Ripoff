140fc7cd2de1848dbb50a8312958f7058422c51c
Mateusz Chochol <mch.priv@gmail.com>
2021-04-29 00:29:14 +0200

    Changing background picture

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -39,7 +39,9 @@ import TripPage from './TripPage/TripPage';
 const styles = {
   paperContainer: {
     //background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,223,166,1) 0%, rgba(0,119,232,1) 100%)',
-    backgroundImage: `url(${process.env.PUBLIC_URL}/img.jpg)`
+    backgroundImage: `url(${process.env.PUBLIC_URL}/background.jpg)`,
+    backgroundSize: 'cover',
+    backgroundPosition: '50% 50%'
   }
 };
 
