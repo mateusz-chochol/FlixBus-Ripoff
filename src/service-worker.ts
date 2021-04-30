8c857a34602426ff15c2610caee241dc99f1ae02
Mateusz Chochol <mch.priv@gmail.com>
2021-04-30 14:56:47 +0200

    Adding more file extentions to service workers

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -57,7 +57,8 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  ({ url }) => url.origin === self.location.origin &&
+    (url.pathname.endsWith('.png') || url.pathname.endsWith('.jpg') || url.pathname.endsWith('.webp') || url.pathname.includes('/icons/') || url.pathname.includes('/map_markers/')),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
