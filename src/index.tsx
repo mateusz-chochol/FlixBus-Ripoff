a6d2d479be2794b0eb5afe5713893952d44ff3f9
Mateusz Chochol <mch.priv@gmail.com>
2021-04-30 03:46:35 +0200

    Adding service worker and manifest.json

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from 'contexts/AuthContext';
 import { SnackbarProvider } from 'notistack';
 import { Provider } from 'react-redux';
 import store from 'redux/Store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,3 +20,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
