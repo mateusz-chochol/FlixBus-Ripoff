43a170016ba1f86616c7e0c3bdb4b172e22a18c0
Mateusz Chochol <mch.priv@gmail.com>
2021-05-14 22:25:14 +0200

    Making position of the svg graphic fixed

--- src/components/ServicesPage/ServicesPage.tsx
+++ src/components/ServicesPage/ServicesPage.tsx
@@ -18,7 +18,8 @@ import { ReactComponent as ServicesSvg } from 'svgs/services.svg';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     parentGrid: {
-      minHeight: "calc(100vh - 75px)",
+      height: "calc(100vh - 75px)",
+      maxWidth: '100vw'
     },
     paper: {
       overflow: 'auto',
@@ -41,45 +42,53 @@ const ServicesPage: React.FC<WithWidth> = ({ width }) => {
 
   return (
     <Paper square className={classes.paper}>
-      <Box paddingLeft={5} paddingRight={5} paddingTop={2} paddingBottom={2}>
+      <Box paddingTop={2} paddingBottom={2}>
         <Grid container className={classes.parentGrid} alignItems='center' justify='center'>
           <Grid item xs={isSmallScreen ? 12 : 5}>
-            <Box>
+            <Box paddingBottom={3}>
               <Typography variant='h2' align='center' color='textSecondary'>Services</Typography>
             </Box>
-            <Box padding={3}>
+            <Box paddingTop={3} paddingBottom={3} paddingLeft={5} paddingRight={5}>
               <Typography variant='h4' gutterBottom>
                 User
-            </Typography>
+              </Typography>
               <Typography variant='subtitle1' gutterBottom>
                 App allows to register a user via email and password and then log in as one. After logging in user grants access to changing their profile info, looking at their history and syncing carts between muttiple browsers and tabs.
-            </Typography>
+              </Typography>
             </Box>
-            <Box padding={3}>
+            <Box paddingTop={3} paddingBottom={3} paddingLeft={5} paddingRight={5}>
               <Typography variant='h4' gutterBottom>
                 Searching
-            </Typography>
+              </Typography>
               <Typography variant='subtitle1' gutterBottom>
                 User can either use main page search bar to search for trips with given date and departure and destination places or they might use route map page to display all accessible locations and select them via clicking on map markers (or by using search box that behaves similary to the main page one)
-            </Typography>
+              </Typography>
             </Box>
-            <Box padding={3}>
+            <Box paddingTop={3} paddingBottom={3} paddingLeft={5} paddingRight={5}>
               <Typography variant='h4' gutterBottom>
                 Main flow
-            </Typography>
+              </Typography>
               <Typography variant='subtitle1' gutterBottom>
                 After finding appropriate trips user can add them to their cart (which is going to be saved on their user profile granted that they are logged in) and from there by clicking the checkout button user can proceed to simulate seats reservation on the selected trips.
-            </Typography>
+              </Typography>
+            </Box>
+            <Box paddingTop={3} paddingBottom={3} paddingLeft={5} paddingRight={5}>
+              <Typography variant='h4' gutterBottom>
+                Data
+              </Typography>
+              <Typography variant='subtitle1' gutterBottom>
+                Website uses firebase as a backend service for both authentication and for storing any user data. If you can see error snackbars showing after trying to logged in it means that API keys have probably expired and the site cannot contact the backend.
+              </Typography>
             </Box>
             <Hidden mdUp>
-              <Box display='flex' justifyContent='center' paddingBottom={2}>
+              <Box display='flex' justifyContent='center' paddingTop={3} paddingBottom={3} paddingRight={3}>
                 <ServicesSvg width='95%' height='100%' />
               </Box>
             </Hidden>
           </Grid>
           <Hidden smDown>
             <Grid item xs={7}>
-              <Box display='flex' paddingRight={2}>
+              <Box display='flex' position='fixed' top='20%' width='55%'>
                 <ServicesSvg />
               </Box>
             </Grid>
