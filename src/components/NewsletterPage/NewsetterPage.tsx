e2dba2c245234bae29256c2977d8f23d1d9b9da8
Mateusz Chochol <mch.priv@gmail.com>
2021-05-15 03:51:06 +0200

    Changing background color for every page

--- src/components/NewsletterPage/NewsetterPage.tsx
+++ src/components/NewsletterPage/NewsetterPage.tsx
@@ -4,11 +4,41 @@ import {
   // Typography,
   Paper,
 } from '@material-ui/core';
+import {
+  createStyles,
+  makeStyles,
+  Theme
+} from '@material-ui/core/styles';
 import { ReactComponent as NewsletterSvg } from 'svgs/newsletter.svg';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      overflow: 'auto',
+      '&::-webkit-scrollbar': {
+        width: '0.4em'
+      },
+      '&::-webkit-scrollbar-track': {
+        '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+      },
+      '&::-webkit-scrollbar-thumb': {
+        backgroundColor: 'rgba(0,0,0,.2)',
+      },
+      backgroundSize: 'cover',
+      backgroundPosition: '50% 50%',
+      backgroundColor: 'whitesmoke'
+    },
+    fixedItem: {
+      pointerEvents: 'none'
+    },
+  }),
+);
+
 const NewsetterPage: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <Paper square>
+    <Paper square className={classes.paper}>
       <Box
         display="flex"
         justifyContent="center"
