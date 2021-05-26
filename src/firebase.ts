b254139ab07f14c18f2a202ac89c57a2631abb30
Mateusz Chochol <mch.priv@gmail.com>
2021-05-27 00:55:24 +0200

    Add google analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/analytics';
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,6 +15,7 @@ const app = firebase.initializeApp({
 
 export const auth = app.auth();
 export const firestore = app.firestore();
+export const analytics = app.analytics();
 
 if (window.location.hostname.includes('localhost')) {
   auth.useEmulator("http://localhost:9099")
